1326f34e9a905873941e11bce8d8d55b2c70e032
vipul <lupinworld1243@gmail.com>
2023-11-11 16:15:47 +0530

    add filter and add subject functionality

--- src/pages/get-student-ids/GetStudent.js
+++ src/pages/get-student-ids/GetStudent.js
@@ -12,7 +12,10 @@ const GetStudent = () => {
   //   const { courseName, semesterNumber, subjectName } = useParams();
 
   const [selectedCategory, setSelectedCategory] = useState("");
+  const [searchEnrollNo, setSearchEnrollNo] = useState("");
+  const [filter, setFilter] = useState(false);
   const [courses, setCourses] = useState([]);
+  const [data, setData] = useState([]);
   let [id, setId] = useState([]);
   if (!id) {
     id = [];
@@ -21,6 +24,25 @@ const GetStudent = () => {
 
   const navigateToStudentResult = (id) => {
     // navigate(`/student-result/${id}/${semesterNumber}/${courseName}`);
+  };
+
+  //  detail.filter((student) => {
+  //   console.log(student);
+  //   // return student.enrollNo && student.enrollNo.includes(searchEnrollNo);
+  // });
+
+  const filteredStudents = () => {
+    const filteredStudent = detail.filter((student) => {
+      // console.log(student);
+      return student.enrollNo && student.enrollNo.includes(searchEnrollNo);
+    });
+    setFilter(true);
+    setData(filteredStudent);
+    // console.log(filteredStudent)
+  };
+
+  const handleSearchEnrollNoChange = (event) => {
+    setSearchEnrollNo(event.target.value);
   };
 
   const getDetailsById = async (x) => {
@@ -59,7 +81,7 @@ const GetStudent = () => {
       dispatch(SetLoading(false));
       if (response.data.success) {
         message.success(response.data.message);
-        setDetail([])
+        setDetail([]);
         response.data.data.map((xx) => {
           getDetailsById(xx);
         });
@@ -113,8 +135,10 @@ const GetStudent = () => {
     getAllStudentIds();
   }, [selectedCategory]);
 
+  // console.log(data);
+
   return (
-    <div>
+    <div className="set-width">
       <div className="course-appbar-header">
         <h2>All Students</h2>
       </div>
@@ -132,31 +156,88 @@ const GetStudent = () => {
         </select>
       </div>
 
+      {id.length !== 0 && (
+        <div className="aaa">
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search by Enroll No"
+              value={searchEnrollNo}
+              onChange={handleSearchEnrollNoChange}
+            />
+            <button onClick={filteredStudents}>Click</button>
+          </div>
+          <button
+            onClick={() => {
+              setFilter(false);
+              setSearchEnrollNo("");
+            }}
+          >
+            Show all students
+          </button>
+        </div>
+      )}
+
       <div className="grid-container">
-        {id.map((value, index) => {
-          return (
-            <div className="grid-item" key={index}>
-              <div>
-                {detail[index] && (
-                  <div
-                    className="ph-course-parent"
-                    onClick={() => navigateToStudentResult(detail[index]._id)}
-                  >
-                    <p className="set-index">{index + 1}.</p>
-                    <img
-                      src={`http://localhost:9000/public/${detail[index].imageFile}`}
-                      alt="success"
-                    />
-                    <div className="ph-course-detail">
-                      <h3>Name: {detail[index].studentName}</h3>
-                      <h3>EnrollNo : {detail[index].enrollNo}</h3>
-                    </div>
+        {id.length === 0 && (
+          <div className="text-set">
+            <p>
+              No Student Registered with this course or matching the search
+              criteria
+            </p>
+          </div>
+        )}
+
+        {filter && data.length === 0 && (
+          <div className="text-set">
+            <p>No Student Found</p>
+          </div>
+        )}
+
+        {filter &&
+          data.map((value, index) => {
+            return (
+              <div className="grid-item" key={index}>
+                <div className="ph-course-parent">
+                  <p className="set-index">{index + 1}.</p>
+                  <img
+                    src={`http://localhost:9000/public/${value.imageFile}`}
+                    alt="success"
+                  />
+                  <div className="ph-course-detail">
+                    <h3>Name: {value.studentName}</h3>
+                    <h3>EnrollNo : {value.enrollNo}</h3>
                   </div>
-                )}
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+
+        {!filter &&
+          id.map((value, index) => {
+            return (
+              <div className="grid-item" key={index}>
+                <div>
+                  {detail[index] && (
+                    <div
+                      className="ph-course-parent"
+                      onClick={() => navigateToStudentResult(detail[index]._id)}
+                    >
+                      <p className="set-index">{index + 1}.</p>
+                      <img
+                        src={`http://localhost:9000/public/${detail[index].imageFile}`}
+                        alt="success"
+                      />
+                      <div className="ph-course-detail">
+                        <h3>Name: {detail[index].studentName}</h3>
+                        <h3>EnrollNo : {detail[index].enrollNo}</h3>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
